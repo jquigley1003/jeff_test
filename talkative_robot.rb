ae329adbfeb8cd1cb92841ec492e2019d77917c4
jquigley1003 <jeff.quigley@wavinghi.com>
2014-09-24 09:44:59 -0400

    Finished lab 02 exercises

--- talkative_robot.rb
+++ talkative_robot.rb
@@ -3,14 +3,58 @@ require 'pry'
 puts "What is your name?"
 user_name = gets.chomp
 puts "How old are you?"
-user_age = gets.chomp
+user_age = gets.chomp.to_i
+puts "What is your gender? (M or F)"
+user_gender = gets.chomp.upcase!
 
 puts "Hi #{user_name} who is #{user_age} years old!"
 
+puts "Hey, you are male." if user_gender == "M"
+
+puts "Hey, you are female" unless user_gender == "M"  
+
+if user_gender == "M"
+	puts "You are probably handsome, too!"
+else
+	puts "You are probably beautiful, too!"
+end
+
+case
+when user_age < 75
+	puts "You'll be 75 in #{75 - user_age} years"
+when user_age == 75
+	puts "You're 75!"
+when user_age > 75
+	puts "You turned 75 #{user_age - 75} years ago."
+end
+
+if user_age < 30 && user_gender == "M"
+	puts "You are a young boy."
+elsif user_age < 30 && user_gender == "F"
+	puts "You are a young girl."
+elsif user_age > 30 && user_gender == "M"
+	puts "You are a mature man."
+else
+	puts "You are a mature woman."
+end
+
+if user_age > 100 && user_gender == "M"
+	puts "Are you a great-great grandfather?"
+elsif user_age > 100 && user_gender == "F"
+	puts "Are you a great-great grandmother?"
+end
+
+puts user_age < 100 ? "You will reach 100 in #{100 - user_age} years" : "You are 100+!"
+
 initial = user_name.chars.first
 puts "Do you mind if I call you #{initial}?"
-future_age = 75 - user_age.to_i
-puts "You will be 75 years old in #{future_age} years."
+
+future_age = 75 - user_age
+if future_age > 0
+	puts "You will be 75 years old in #{future_age} years."
+else
+	puts "You already attained that amazing age of 75!"
+end
 
 user_name.upcase!
 
